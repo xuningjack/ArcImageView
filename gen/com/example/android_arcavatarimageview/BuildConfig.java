7ac2853f380bb76c691b79b824a30664776fbc8b
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-03-01 09:47:48 +0800

    delete gen and bin

--- gen/com/example/android_arcavatarimageview/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android_arcavatarimageview;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
